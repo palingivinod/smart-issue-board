7e8b19f2dbeb1671ba119560cff910416c3c85e0
Palingi Vinod <167117640+palingivinod@users.noreply.github.com>
2025-12-30 12:16:41 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
+import Header from "./components/header";
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
